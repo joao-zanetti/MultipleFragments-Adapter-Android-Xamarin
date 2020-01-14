0c7082b9f27f0649d2f832dacce64b2f5cbb3d3e
joao-zanetti <57362590+joao-zanetti@users.noreply.github.com>
2020-01-14 14:56:04 -0300

    Add files via upload

---
+++ Adapter_Fragment.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using static Android.Graphics.ColorSpace;
+
+
+
+namespace Fragment
+{
+    class Adapter_Fragment : FragmentPagerAdapter
+    {
+
+        public string[] titles = new string[] { "Tab1", "Tab2" };
+        List<Android.Support.V4.App.Fragment> fgls = new List<Android.Support.V4.App.Fragment>();
+
+        public Adapter_Fragment(Android.Support.V4.App.FragmentManager fm)
+            : base(fm)
+        {
+            fgls.Add(Fragment_1.NewInstance());
+            fgls.Add(Fragment_2.NewInstance());
+        }
+
+        public override int Count
+        {
+            get { return titles.Length; }
+        }
+
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+            return fgls[position];
+        }
+
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+            return new Java.Lang.String(titles[position]);
+        }
+
+
+    }
+}
+
---
+++ MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using System;
+
+namespace Fragment
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ViewPager viewPager;
+        Button buttonFrag1;
+        Button buttonFrag2;
+        Adapter_Fragment adapter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            viewPager = FindViewById<ViewPager>(Resource.Id.viewP);
+            buttonFrag1 = FindViewById<Button>(Resource.Id.buttonFrag1);
+            buttonFrag2 = FindViewById<Button>(Resource.Id.buttonFrag2);
+
+            buttonFrag1.Click += clickFrag1;
+            buttonFrag2.Click += clickFrag2;
+
+            adapter = new Adapter_Fragment(SupportFragmentManager);     
+        }
+
+        private void clickFrag1(object sender, EventArgs e)
+        {
+            viewPager.Adapter = adapter;
+            viewPager.SetCurrentItem(0, true);
+        }
+
+        private void clickFrag2(object sender, EventArgs e)
+        {
+            viewPager.Adapter = adapter;
+            viewPager.SetCurrentItem(1, true);
+        }
+
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
